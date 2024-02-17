da59b5df8c25dd0a532b95815aaaf8e151156cf1
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-17 19:34:02 -0300

    feat: create slide-panel and use this in todo component (44:28)

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoCardComponent } from '../../shared/components/todo-card/todo-card.component';
+import { TodoStatus, TodoCardComponent } from '../../shared/components/todo-card/todo-card.component';
 import { TodoService } from '../../core/services/todo.service';
 import { ITodo } from '../../core/models/todo.model';
+import { SlidePanelComponent } from '../../shared/ui/slide-panel/slide-panel.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [TodoCardComponent],
+  imports: [TodoCardComponent, SlidePanelComponent, ReactiveFormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
 export class TodoComponent implements OnInit {
+  todoForm!: FormGroup;
   todos: ITodo[] = [];
+  todoStatus = TodoStatus;
+  isSlidePanelOpen = false;
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService, private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      status: new FormControl('OPEN', [Validators.required]),
+    });
+  }
 
   ngOnInit(): void {
     this.getAllTodos();
@@ -23,5 +34,19 @@ export class TodoComponent implements OnInit {
     this.todos = this.todoService.getAllTodos();
   }
 
-  openSlidePanel() {}
+  openSlidePanel() {
+    this.isSlidePanelOpen = true;
+  }
+
+  onCloseSlidePanel() {
+    this.isSlidePanelOpen = false;
+  }
+
+  onSubmit() {
+    if (this.todoForm.valid) {
+      //
+    } else {
+      this.todoForm.markAllAsTouched();
+    }
+  }
 }
