53a219dfe26cc983039d13a53c0b67ceb230dc50
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-17 13:37:49 -0300

    feat: create routes and pages structure (9:23)

--- src/app/shared/layouts/default/default.component.ts
+++ src/app/shared/layouts/default/default.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './default.component.html',
-  styleUrl: './default.component.scss'
+  styleUrl: './default.component.scss',
 })
-export class DefaultComponent {
-
-}
+export class DefaultComponent {}
