27feb65c7e7c5df3db8653fd438793abc14016ff
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-17 19:50:32 -0300

    feat: create auth service (55:48)

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { ILogin, ILoginResponse } from '../models/auth.model';
+import { HttpClient } from '@angular/common/http';
+import { apiEndpoint } from '../constants/constants';
+import { map } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private httpClient: HttpClient, private tokenService: TokenService) {}
 
-  constructor() { }
+  onLogin(data: ILogin) {
+    return this.httpClient.post<ILoginResponse>(`${apiEndpoint.AuthEndpoint.login}`, data).pipe(
+      map((response) => {
+        if (response) {
+          this.tokenService.setToken(response.token);
+        }
+
+        return response;
+      })
+    );
+  }
+
+  onLogout() {
+    this.httpClient.post(`${apiEndpoint.AuthEndpoint.logout}`, '').subscribe({
+      next: () => {
+        this.tokenService.removeToken();
+      },
+    });
+  }
 }
