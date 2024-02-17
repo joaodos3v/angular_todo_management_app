25318abd4a22b81ce5d2fff9e493b0de7c411a61
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-17 19:59:33 -0300

    feat: create http interceptor (1:00:39)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(BrowserAnimationsModule)],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([httpInterceptor])),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 };
