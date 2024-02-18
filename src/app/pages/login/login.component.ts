72b12c6e419cac90d9b72f03ed0a646c1a35cabd
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-17 22:07:56 -0300

    feat: implement route guards (1:05:13)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +12,7 @@ import {
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
@@ -26,6 +21,9 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
+      this.authService.onLogin(this.loginForm.value).subscribe({
+        next: () => {},
+      });
     } else {
       this.loginForm.markAllAsTouched();
     }
