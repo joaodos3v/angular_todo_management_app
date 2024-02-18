20f8bd6d397531d8b8b05d0fef99cc27e3e6e877
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-17 22:14:53 -0300

    feat: add logout option (1:08:57)

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { TokenService } from '../../../core/services/token.service';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isAuthenticated$;
 
+  constructor(private tokenService: TokenService, private authService: AuthService) {
+    this.isAuthenticated$ = this.tokenService.isAuthentication;
+  }
+
+  onLogout() {
+    this.authService.onLogout();
+  }
 }
